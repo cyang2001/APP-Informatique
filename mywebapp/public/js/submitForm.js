5b4ab3f8a28046e329968520981e03f5d35468f3
Chen YANG <gregyoungforever@gmail.com>
2024-04-05 01:45:20 +0200

    update js

--- mywebapp/public/js/submitForm.js
+++ mywebapp/public/js/submitForm.js
@@ -2,7 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
   var submitButton = document.querySelector('.btn');
   if (submitButton) {
       submitButton.addEventListener('click', function() {
+          if (document.getElementById('login-form')) {
           var form = document.getElementById('login-form');
+          } else if (document.getElementById('register-form')) {
+          var form = document.getElementById('register-form');
+          }
           if (form) { 
               form.submit();
           }
