3d9f443e27244d34fa8a3d40361a0f7873b41046
Chen YANG <gregyoungforever@gmail.com>
2024-04-18 23:03:05 +0200

    feature Chen yang add login feature

---
+++ mywebapp/public/js/login.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('login-form');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // prevent the form from submitting
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+
+        const formData = new FormData(form);
+        fetch('index.php?action=login', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.success) {
+                // delete alert if needs to be removed
+                alert('Connexion réussie!');
+                // redirect to home page
+                window.location.href = '../homePage.html';
+            } else {
+                alert('Erreur: ' + data.message);
+                // display error message
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Erreur: Vérifiez la console pour plus de détails');
+        });
+    });
+});
