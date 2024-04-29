475bb1e0b28477a9b207b40572f156bb22e08393
Chen YANG <gregyoungforever@gmail.com>
2024-04-29 16:08:03 +0200

    update

---
+++ mywebapp/src/config/database.php
@@ -0,0 +1,43 @@
+<?php
+class Database {
+    private static $instance = null;
+    private $pdo;
+
+    private function __construct() {
+        $host = '127.0.0.1';
+        $db   = 'APP';
+        $user = 'root';
+        $pass = '123456';
+        $charset = 'utf8mb4';
+
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        try {
+            $this->pdo = new PDO($dsn, $user, $pass, $options);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+
+    public static function getInstance() {
+        if (!self::$instance) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->pdo;
+    }
+
+    // Prevent the instance from being cloned 
+    private function __clone() { }
+
+    // Prevent from being unserialized 
+    private function __wakeup() { }
+}
