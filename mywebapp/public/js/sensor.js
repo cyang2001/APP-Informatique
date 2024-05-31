72e879b805b6854b0de6e110f8070e87bee5ce27
Chen YANG <gregyoungforever@gmail.com>
2024-05-31 14:55:25 +0200

    update

--- mywebapp/public/js/sensor.js
+++ mywebapp/public/js/sensor.js
@@ -11,7 +11,6 @@ document.addEventListener('DOMContentLoaded', function() {
                     return e.niveau_db;
                 });
 
-
                 var ctxSound = document.getElementById('soundLevelChart').getContext('2d');
                 var soundLevelChart = new Chart(ctxSound, {
                     type: 'line',
@@ -21,10 +20,14 @@ document.addEventListener('DOMContentLoaded', function() {
                             label: 'Niveau Sonore (dB)',
                             data: niveaux_db,
                             borderColor: 'rgb(255, 99, 132)',
-                            fill: false
+                            fill: false,
+                            pointRadius: 5,
+                            pointHoverRadius: 7
                         }]
                     },
                     options: {
+                        responsive: true,
+                        maintainAspectRatio: false,
                         scales: {
                             x: {
                                 type: 'time',
@@ -35,6 +38,16 @@ document.addEventListener('DOMContentLoaded', function() {
                             y: {
                                 beginAtZero: true
                             }
+                        },
+                        plugins: {
+                            tooltip: {
+                                enabled: true,
+                                callbacks: {
+                                    label: function(context) {
+                                        return context.dataset.label + ': ' + context.raw + ' dB';
+                                    }
+                                }
+                            }
                         }
                     }
                 });
@@ -43,4 +56,44 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         })
         .catch(error => console.error('Error fetching data:', error));
+});
+
+
+
+window.onload = function() {
+    fetch('index.php?action=getUserInfo', {
+        method: 'GET'
+    })
+    .then(response => response.json()) 
+    .then(data => { 
+        console.log(data);  
+        const menu = document.querySelector('.menu-deroulant div');
+        menu.innerHTML = ''; 
+        
+
+        if (data.email) {
+            menu.innerHTML = '<a href="#" id="logout">Se déconnecter</a>';
+            document.getElementById('logout').addEventListener('click', function(event) {
+                event.preventDefault();
+                fetch('index.php?action=logout', {
+                    method: 'GET'
+                })
+                .then(() => location.reload())
+                .catch(error => console.error('Error on logout:', error));
+            });
+        } else {
+            menu.innerHTML = '<a href="./login_fr.html">Se connecter</a><a href="./register_fr.html">S\'inscrire</a>';
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching user info:', error);
+    });
+};
+
+
+document.addEventListener('scroll', function() {
+    const parallaxElement = document.querySelector('.banniere img');
+    const speed = 0.5;
+    var windowScroll = window.pageYOffset;
+    parallaxElement.style.transform = 'translateY(' + windowScroll * speed + 'px)';
 });
